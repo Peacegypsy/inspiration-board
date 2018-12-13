22d135798c52e3e78073033a9fdd75d1d0d43778
Xtina <2006peacegypsy@gmail.com>
2018-12-13 12:34:41 -0800

    displaying cards

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,33 +1,36 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
-import './Board.css';
-import Card from './Card';
-import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
+import "./Board.css";
+import Card from "./Card";
+import NewCardForm from "./NewCardForm";
+import CARD_DATA from "../data/card-data.json";
 
 class Board extends Component {
   constructor() {
     super();
 
     this.state = {
-      cards: [],
+      cards: []
     };
   }
+  populateCards = () => {
+    return CARD_DATA["cards"].map(card => {
+      return <Card id={card.id} text={card.text} emoji={card.emoji} />;
+    });
+  };
 
   render() {
     return (
       <div>
-        Board
+        <div>Board</div>
+        <div>{this.populateCards()}</div>
       </div>
-    )
+    );
   }
-
 }
 
-Board.propTypes = {
-
-};
+Board.propTypes = {};
 
 export default Board;
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import emoji from 'emoji-dictionary';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import emoji from "emoji-dictionary";
 
-import './Card.css';
+import "./Card.css";
 
 class Card extends Component {
   render() {
     return (
-      <div className="card">
+      <div className="card" key={this.props.id}>
+        <p>{this.props.text}</p>
+        <p>{this.props.emoji} </p>
+        <p>{}</p>
+        <p />
         Card
       </div>
-    )
+    );
   }
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string.isRequired,
+  emoji: PropTypes.string,
+  id: PropTypes.number.isRequired
 };
 
 export default Card;
